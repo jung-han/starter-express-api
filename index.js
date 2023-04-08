e583480c0b106d13a4b608507fb882490591b1f7
hanjung <hanjungv@gmail.com>
2023-04-08 17:39:57 +0900

    change categories, change shoes image

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const products = require("./products.json");
+const categories = require("./categories.json");
 const app = express();
 
 app.get("/products", (req, res) => {
+  console.log(req, "req");
   res.send(products);
+});
+
+app.get("/categories", (req, res) => {
+  console.log(req, "req");
+  res.send(categories);
 });
 
 app.listen(process.env.PORT || 3000);
